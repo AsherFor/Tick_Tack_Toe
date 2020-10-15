17d34cd16371ceaece8f4ae07a2d9326adffbf64
AsherFor <asherforman@stu.postoakschool.org>
2020-10-15 09:47:09 -0700

    Final Commit

--- Tick_Tack_Toe.py
+++ Tick_Tack_Toe.py
@@ -1,6 +1,6 @@
 '''
 Asher Forman
-Tick Tack Toe Board
+Tick Tack Toe
 10/1/20
 '''
 # Welcome Code
@@ -21,7 +21,6 @@ win_condition = False
 print("Welcome to tick tack toe!")
 #How do I make the code for 0 or 1 players.
 num_of_players = input("Choose the number of players. Valid inputs are: 0, 1, or 2. ")
-#What do I do for 0 players?
 #What do I do if there is only 1 player?
 print(f'You have chosen {num_of_players} players.')
 player1_x_or_o = input("Chose X or O. ")
@@ -37,7 +36,7 @@ else:
     print("Player 1 is O's and player 2 is X's")
 
 board = [
-    [" ", " ", ""],
+    [" ", " ", " "],
     [" ", " ", " "],
     [" ", " ", " "]
 ]
@@ -50,12 +49,14 @@ def player_1_turn():
     int_player1_row = int(player1_row)
     int_player1_col = int(player1_col)
 
-    #How do I check to see if a spot was already taken.
-
+    if board[int_player1_row][int_player1_col] == "X" or board[int_player1_row][int_player1_col] == "O":
+        print("This space has already been used, please choose again.")
+        player_1_turn()
 
     print(f'Player 1 picked row {int_player1_row} and column {int_player1_col}')
 
-    board[int_player1_row] [int_player1_col] = player1_x_or_o
+    board[int_player1_row][int_player1_col] = player1_x_or_o
+
 
 #Player 2 picks their row and column
 def player_2_turn():
@@ -65,12 +66,17 @@ def player_2_turn():
     int_player2_row = int(player2_row)
     int_player2_col = int(player2_col)
 
+    if board[int_player2_row][int_player2_col]  == "X" or board[int_player2_row][int_player2_col] == "O":
+        print("This space has already been used, please choose again.")
+        player_2_turn()
+
     print(f'Player 2 picked row {int_player2_row} and column {int_player2_col}')
-    board[int_player2_row] [int_player2_col] = player2_x_or_o
+    board[int_player2_row][int_player2_col] = player2_x_or_o
 
 
 #How do I clear the board when I want to play again?
-def clear_board():
+def new_board():
+    global board
     board = [
         [" ", " ", ""],
         [" ", " ", " "],
@@ -84,34 +90,34 @@ def didIWin():
         win_condition = True
 
     #Column 0 Down
-    if board[0][0] == board[1][0] and board[0][0] == board[2][0] and board[0][0] != " ":
+    elif board[0][0] == board[1][0] and board[0][0] == board[2][0] and board[0][0] != " ":
         win_condition = True
 
     #Row 1 Across
-    if board[1][0] == board[1][1] and board[1][0] == board[1][2] and board[1][0] != " ":
+    elif board[1][0] == board[1][1] and board[1][0] == board[1][2] and board[1][0] != " ":
         win_condition = True
 
     #Column 1 Down
-    if board[0][1] == board[1][1] and board[0][1] == board[2][1] and board[0][1] != " ":
+    elif board[0][1] == board[1][1] and board[0][1] == board[2][1] and board[0][1] != " ":
         win_condition = True
 
     #Row 2 Across
-    if board[2][0] == board[2][1] and board[2][0] == board[2][2] and board[2][0] != " ":
+    elif board[2][0] == board[2][1] and board[2][0] == board[2][2] and board[2][0] != " ":
         win_condition = True
 
     #Column 2 Down
-    if board[0][2] == board[1][2] and board[0][2] == board[2][2] and board[0][2] != " ":
+    elif board[0][2] == board[1][2] and board[0][2] == board[2][2] and board[0][2] != " ":
         win_condition = True
 
     #Diagonal Starting with Row and Column 0
-    if board[0][0] == board[1][1] and board[0][0] == board[2][2] and board[0][0] != " ":
+    elif board[0][0] == board[1][1] and board[0][0] == board[2][2] and board[0][0] != " ":
         win_condition = True
 
     #Diagonal Starting Row 0 Column 2
-    if board[0][2] == board[1][1] and board[0][2] == board[2][0] and board[0][2] != " ":
+    elif board[0][2] == board[1][1] and board[0][2] == board[2][0] and board[0][2] != " ":
         win_condition = True
     #How do I make a tie condition?
-    # if board[0, 2] and board[0, 2] == "X" or "O":
+    # elif board[0][0] == "X" or "O" and board[0][1] == "X" or "O" and board[0][2] == "X" or "O" and board[1][0] == "X" or "O" and board[1][1] == "X" or "O" and board[1][2] == "X" or "O" and board[2][0] == "X" or "O" and board[2][1] == "X" or "O" and board[2][2] == "X" or "O":
     #     print("Tie Game!")
     else:
         print("No one has one yet, keep playing.")
@@ -131,31 +137,36 @@ def drawBoard():
 
 
 
-#How do I make it so that game plays again?
-while win_condition == False:
-    player_1_turn()
-    drawBoard()
-    didIWin()
-    if win_condition == True:
-        print("Congratulations, Player 1 won the Game!")
-        print("♫♪.ılılıll|̲̅̅●̲̅̅|̲̅̅=̲̅̅|̲̅̅●̲̅̅|llılılı.♫♪")
-        play_again_or_end = input("Type y to play again, or type n to stop.")
-        if play_again_or_end == "y" or "Y":
-            break
-        else:
-            print("Game over!")
-            exit()
-    player_2_turn()
-    drawBoard()
-    didIWin()
-    if win_condition == True:
-        print("Congratulations, Player 2 won the Game")
-        print("♫♪.ılılıll|̲̅̅●̲̅̅|̲̅̅=̲̅̅|̲̅̅●̲̅̅|llılılı.♫♪")
-        play_again_or_end = input("Type y to play again, or type n to stop.")
-        if play_again_or_end == "y" or "Y":
-            break
-        else:
-            print("Game over!")
-            exit()
+def game_play():
+    global win_condition
+    while win_condition == False:
+        player_1_turn()
+        drawBoard()
+        didIWin()
+        if win_condition == True:
+            print("Congratulations, Player 1 won the Game!")
+            print("♫♪.ılılıll|̲̅̅●̲̅̅|̲̅̅=̲̅̅|̲̅̅●̲̅̅|llılılı.♫♪")
+            play_again_or_end = input("Type y to play again, or type n to stop. ")
+            if play_again_or_end == "y" or play_again_or_end == "Y ":
+                new_board()
+                win_condition = False
+                game_play()
+            else:
+                print("Game over!")
+                exit()
+        player_2_turn()
+        drawBoard()
+        didIWin()
+        if win_condition == True:
+            print("Congratulations, Player 2 won the Game!")
+            print("♫♪.ılılıll|̲̅̅●̲̅̅|̲̅̅=̲̅̅|̲̅̅●̲̅̅|llılılı.♫♪")
+            play_again_or_end = input("Type y to play again, or type n to stop. ")
+            if play_again_or_end == "y" or play_again_or_end == "Y":
+                new_board()
+                win_condition = False
+                game_play()
+            else:
+                print("Game over!")
+                exit()
 
-
+game_play()
